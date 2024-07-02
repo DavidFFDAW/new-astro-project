b347bad045a218eda4c36c5758e4a86f57925482
DavidFFDAW <davidferflo2@gmail.com>
2024-07-02 11:37:38 +0200

    feat: manifest and changes

--- src/database/db.js
+++ src/database/db.js
@@ -2,10 +2,10 @@ import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 
 const connection = await mysql.createConnection({
-    host: '',
-    user: '',
-    database: '',
-    password: '',
+    host: import.meta.env.DB_HOST,
+    user: import.meta.env.DB_USER,
+    database: import.meta.env.DB_NAME,
+    password: import.meta.env.DB_PASSWORD,
 });
 
 export const db = drizzle(connection);
