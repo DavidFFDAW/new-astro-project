e60737be5e62d1ff7475d3d4064797552afb10cd
DavidFFDAW <davidferflo2@gmail.com>
2024-07-02 09:24:35 +0200

    fix: vercel plugin and tsconfig error fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from 'astro/config';
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from 'vite-plugin-pwa';
+import vercelServerless from '@astrojs/vercel/serverless';
+// import node from "@astrojs/node";
 
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
-  plugins: [
-    VitePWA({ registerType: 'autoUpdate' })
-  ]
+  adapter: vercelServerless(),
+  plugins: [VitePWA({
+    registerType: 'autoUpdate'
+  })]
 });
