8a7b7fd212a3e2d9a7f7d82de162d58d096a92d3
DavidFFDAW <davidferflo2@gmail.com>
2024-07-05 13:10:25 +0200

    cli: configurations and new libraries

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import { VitePWA } from 'vite-plugin-pwa';
 import vercelServerless from '@astrojs/vercel/serverless';
 // import node from "@astrojs/node";
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercelServerless(),
-  plugins: [VitePWA({
-    registerType: 'autoUpdate'
-  })],
-  integrations: [react()]
+    output: 'server',
+    adapter: vercelServerless(),
+    plugins: [
+        VitePWA({
+            registerType: 'autoUpdate',
+        }),
+    ],
+    integrations: [react()],
+    redirects: {
+        '/private': '/private/dashboard',
+    },
 });
